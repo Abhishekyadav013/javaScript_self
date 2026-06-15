364b60ce14a47de9390127f5f31baf404544d9e5
Abhishek Yadav <abhishekyadav44121@gmail.com>
2026-06-15 22:42:32 +0530

    Maths and NUm methods and working

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -12,4 +12,30 @@ const score = 450;
     const otherBalance = 450.957;
     console.log(otherBalance.toPrecision(6))
 
+
+    const hundreds = 1000000;
+    console.log(hundreds.toLocaleString('en-IN'));
     
+
+    //---------math method-------//
+
+    // console.log(Math);// math is a built-in object that provides properties and methods for mathematical constants and functions.
+    // console.log(Math.abs(-5)); // absolute value
+    // console.log(Math.round(4.7)); // round to nearest integer
+    // console.log(Math.ceil(4.5)); // round up to nearest integer
+    // console.log(Math.floor(4.9)); // round down to nearest integer
+    // console.log(Math.min(3,4,6));
+    // console.log(Math.max(3,4,6));
+
+    // console.log(Math.random()); // random number between 0 and 1
+    // console.log(Math.random()*100); // random number between 0 and 100
+    // console.log(Math.floor(Math.random()*100)); // random number between 0 and 100
+    // console.log(Math.floor(Math.random()*100)+1); // random number between 1 and 100 
+
+    console.log(Math.floor(Math.random()*10)+1); // random number between 1 and 10
+
+    const min = 10;
+    const max = 30;
+//---------Important consept of random number between min and max------------//
+    console.log(Math.floor(Math.random()*(max-min) + min)); // random number between 10 and 30
+    console.log(Math.floor(Math.random()*(max-min + 1 ) + min)); 
